eea0560afb1e899bc35bd911aeced2425aab93d9
unknown <miki.klimko@gmail.com>
2020-11-30 23:35:12 +0100

    commit

---
+++ app.js
@@ -0,0 +1,170 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const app = express();
+const nodemailer = require('nodemailer');
+
+
+
+// parsovanie pre nedefinovany problem s registraciou 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// views 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//email prihlasenie
+var transportet = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'DP.Klimko@gmail.com',
+        pass: '12355321'
+    }
+});
+
+//pripojenie databazy 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'emailverifikacia'
+});
+//cookie parser
+app.use(cookieParser());
+connection.connect();
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+// Registracia 
+app.post('/', (req, res) => {
+    //vygenerovanie ciselnej kombinácie k heslu
+    function Store(heslo) {
+        var verify = Math.floor((Math.random() * 10000000) + 1);
+        // odosielanie overovacieho mailu
+        var mailOption = {
+            from: 'miki.klimko@gmail.com',
+            to: `${req.body.Email}`,
+            subject: "Overenie uctu",
+            html: `<h1>ODOSLANE SPRAVNE</h1><br> <a href="http://localhost:5000/overenie/?verify=${verify}"> Pre overenie klikny`
+        }
+        // ulozenie dat
+
+        var userData = { Email: req.body.Email, password: heslo, verification: verify };
+        connection.query("INSERT INTO overenie SET ?", userData, (err, result) => {
+            if (err) {
+                console.log(error)
+            } else {
+                transportet.sendMail(mailOption, (error, info) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        let userdata = {
+                            email: `${req.body.Email}`,
+                        }
+                        
+                        res.cookie("UserInfo", userdata);
+                        res.send("<h1>Email bol poslany </h1>");
+                        console.log("poslane")
+                    }
+                })
+                console.log("Data succesfully insert")
+                
+            }
+        })
+    }
+    //zakodovanie hesla
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(req.body.Heslo, salt, function (err, hash) {
+            if (err) {
+                console.log(err);
+            } else {
+                Store(hash);
+            }
+
+        });
+    });
+
+})
+
+//Verifikacia 
+app.get('/overenie/', (req, res) => {
+    function activateAccount(verification) {
+        if (verification == req.query.verify) {
+            connection.query("UPDATE overenie SET active = ?", "true", (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let userdata = {
+                        email: `${req.body.Email}`,
+                        overenie: "TRUE"
+                    }
+                    res.cookie("UserInfo", userdata);
+                    res.send('<h1>Overenie uctu bolo uspesne</h1>');
+                }
+
+            })
+        } else {
+            res.send("<h1>Verifikacia sa nepodarila </h1>")
+        }
+    };
+    connection.query("SELECT overenie.verification FROM overenie WHERE email = ?", req.cookies.UserInfo.email, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            activateAccount(result[0].verification);
+
+        }
+    })
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+// prihlasenie 
+app.post('/login', (req, res) => {
+    var email = req.body.Email;
+    var pass = req.body.Heslo;
+    
+    function LoginSuccess(){
+        let userdata = {
+            email: `${req.body.Email}`,
+            overenie: "True"
+        }
+        res.cookie("UserInfo", userdata);
+        res.json({ overenie: "true" });
+    }
+    connection.query('SELECT * FROM overenie WHERE email = ?', email, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var hash = result[0].password;
+            bcrypt.compare(pass, hash, function (err, res) {
+                if (err) {
+                    res.json({ msg: "ERROR" })
+                } else {
+                   LoginSuccess(); 
+                }
+            });
+        }
+    })
+})
+
+app.listen(5000);
+
+/*Zoznam uloh:
+Opraviť verifikaciu
+Vytvoriť obnovenie hesla / zaslanie noveho hesla
+Rušenie verifikačneho mailu po určitej dobe 
+*/
